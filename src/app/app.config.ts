1b22a252e027ab0c81f36b1d9d65f81eab718f76
MokkapalliPrudviRaj <uiuxbyprudvi@gmail.com>
2026-02-09 03:22:24 +0530

    hash location

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { LucideAngularModule } from 'lucide-angular';
 import { LUCIDE_ICONS } from './shared/icons/lucide-icons';
 import { importProvidersFrom } from '@angular/core';
@@ -9,7 +9,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     importProvidersFrom(LucideAngularModule.pick(LUCIDE_ICONS)),
   ],
 };
